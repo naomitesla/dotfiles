e0169074cdcc5aee7d32365709af7603ffd610f1
naomi tesla <naomitesla@protonmail.com>
2023-10-03 11:42:38 -0400

    removed redudant bit of code

--- nvim/lua/yayyy-neovimmm/init.lua
+++ nvim/lua/yayyy-neovimmm/init.lua
@@ -16,10 +16,6 @@ vim.opt.mouse = 'a'
 vim.opt.guifont='SF Mono Nerd'
 
 
--- for some reason nvim-tree actlly needs this
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- my themeeee c:
 vim.cmd('colorscheme tokyonight-night')
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
