bb697be2af259ed15c08e442a65533e358354efd
naomi tesla <naomitesla@protonmail.com>
2023-10-03 14:38:53 -0400

    fixed bug with buffer line closing, made some things prettier too c:

--- nvim/lua/yayyy-neovimmm/init.lua
+++ nvim/lua/yayyy-neovimmm/init.lua
@@ -19,4 +19,4 @@ vim.opt.guifont='SF Mono Nerd'
 -- my themeeee c:
 vim.cmd('colorscheme tokyonight-night')
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' }) 
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
